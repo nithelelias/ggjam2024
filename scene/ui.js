6fdf2d024e7669e2215bd499685a015c2b4b42b3
nithel rene elias alvarez <nithel.elias@pragma.com.co>
2024-01-27 03:05:18 -0500

    caption level

--- scene/ui.js
+++ scene/ui.js
@@ -14,7 +14,8 @@ export default class UIScene extends Phaser.Scene {
   update() {
     const main = this.scene.get("main");
     this.caption.setText(`
-    LAUGH LEVEL: ${main.laughPercentage}
+    GAME LEVEL: ${main.level}
+    LAUGH LEVEL: ${main.laughPercentage}%
     POINTER: ${main.input.mousePointer.x} ${main.input.mousePointer.y}
         `);
   }
